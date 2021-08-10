09b67e04d979b28bbf89f1c03c3bbdacf8142bd4
todd flanders <toddfbass@gmail.com>
2021-08-10 11:23:15 -0400

    initial commit

---
+++ CheckoutKata/PriceLookup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckoutKata
+{
+    public class PriceLookup
+    {
+        private Dictionary<string, double> _perUnitProducts = new Dictionary<string, double>();
+        private Dictionary<string, double> _perPoundProducts = new Dictionary<string, double>();
+        public void AddPerUnitProduct(string productName, double price)
+        {
+            if (_perPoundProducts.ContainsKey(productName))
+            {
+                throw new ArgumentException($"{productName} is already priced per pound.  Cannot price per unit.");
+            }
+            _perUnitProducts[productName] = price;
+        }
+
+        public double PricePerUnit(string productName)
+        {
+            if (!_perUnitProducts.ContainsKey(productName))
+            {
+                if (_perPoundProducts.ContainsKey(productName))
+                {
+                    throw new ArgumentException($"{productName} is priced per pound, not per unit");
+                }
+                throw new ArgumentException($"[{productName}] is not a valid product");
+            }
+            return _perUnitProducts[productName];
+        }
+
+        public void AddPerPoundProduct(string productName, double price)
+        {
+            if (_perUnitProducts.ContainsKey(productName))
+            {
+                throw new ArgumentException($"{productName} is already priced per unit.  Cannot price per pound.");
+            }
+            _perPoundProducts[productName] = price;
+        }
+
+        public double PricePerPound(string productName, int pounds)
+        {
+            if (!_perPoundProducts.ContainsKey(productName))
+            {
+                if (_perUnitProducts.ContainsKey(productName))
+                {
+                    throw new ArgumentException($"{productName} is priced per unit, not per pound");
+                }
+                throw new ArgumentException($"[{productName}] is not a valid product");
+            }
+            
+            return _perPoundProducts[productName] * pounds;
+        }
+    }
+}
---
+++ CheckoutKataTests/PriceLookupTests.cs
@@ -0,0 +1,109 @@
+using System;
+using CheckoutKata;
+using Xunit;
+using FluentAssertions;
+
+namespace CheckoutKataTests
+{
+    public class PriceLookupTests
+    {
+        private readonly PriceLookup _prices = new();
+        
+        [Fact]
+        public void AddSoup()
+        {
+            _prices.AddPerUnitProduct("Soup", 1.89);
+            _prices.PricePerUnit("Soup").Should().Be(1.89);
+        }
+        
+        [Fact]
+        public void AddPie()
+        {
+            _prices.AddPerUnitProduct("Pie", 6.99);
+            _prices.PricePerUnit("Pie").Should().Be(6.99);
+        }
+
+        [Fact]
+        public void AddBeef()
+        {
+            _prices.AddPerPoundProduct("Ground Beef", 5.99);
+            _prices.PricePerPound("Ground Beef", 2).Should().Be(11.98);
+        }
+
+        [Fact]
+        public void AddBananas()
+        {
+            _prices.AddPerPoundProduct("Bananas", 2.38);
+            _prices.PricePerPound("Bananas", 3).Should().Be(7.14);
+        }
+
+        [Fact]
+        public void UnknownPerUnitProduct()
+        {
+            void Act() => _prices.PricePerUnit("unknown per unit");
+            var exception = Assert.Throws<ArgumentException>((Action) Act);
+            exception.Message.Should().Be("[unknown per unit] is not a valid product");
+        }
+        
+        [Fact]
+        public void UnknownPerPoundProduct()
+        {
+            void Act() => _prices.PricePerPound("unknown per pound", 3);
+            var exception = Assert.Throws<ArgumentException>((Action) Act);
+            exception.Message.Should().Be("[unknown per pound] is not a valid product");
+        }
+
+        [Fact]
+        public void PerUnitPriceUpdate()
+        {
+            _prices.AddPerUnitProduct("Soup", 1.89);
+            _prices.AddPerUnitProduct("Soup", 1.99);
+            _prices.PricePerUnit("Soup").Should().Be(1.99);
+        }
+
+        [Fact]
+        public void PerPoundPriceUpdate()
+        {
+            _prices.AddPerPoundProduct("Ground Beef", 5.99);
+            _prices.AddPerPoundProduct("Ground Beef", 6.49);
+            _prices.PricePerPound("Ground Beef", 2).Should().Be(12.98);
+        }
+        
+        [Fact]
+        public void WrongApiForPerUnit()
+        {
+            _prices.AddPerUnitProduct("Soup", 1.89);
+            void Act() => _prices.PricePerPound("Soup", 2);
+            var exception = Assert.Throws<ArgumentException>(Act);
+            exception.Message.Should().Be("Soup is priced per unit, not per pound");
+        }
+        
+        [Fact]
+        public void WrongApiForPerPound()
+        {
+            _prices.AddPerPoundProduct("Ground Beef", 5.99);
+            void Act() => _prices.PricePerUnit("Ground Beef");
+            var exception = Assert.Throws<ArgumentException>(Act);
+            exception.Message.Should().Be("Ground Beef is priced per pound, not per unit");
+        }
+        
+        [Fact]
+        public void PerUnitProductIsAlreadyPerPound()
+        {
+            _prices.AddPerPoundProduct("Ground Beef", 5.99);
+            void Act() => _prices.AddPerUnitProduct("Ground Beef", 6.49);
+            var exception = Assert.Throws<ArgumentException>(Act);
+            exception.Message.Should().Be("Ground Beef is already priced per pound.  Cannot price per unit.");
+        }
+        
+        [Fact]
+        public void PerPoundProductIsAlreadyPerUnit()
+        {
+            _prices.AddPerUnitProduct("Soup", 1.89);
+            void Act() => _prices.AddPerPoundProduct("Soup", 1.99);
+            var exception = Assert.Throws<ArgumentException>(Act);
+            exception.Message.Should().Be("Soup is already priced per unit.  Cannot price per pound.");
+        }
+        
+    }
+}
